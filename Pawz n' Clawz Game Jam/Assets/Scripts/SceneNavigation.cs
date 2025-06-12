c788a9aa797efd2acd3a20ed532dedb1773e2dcb
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-06-11 19:57:30 -0500

    Navigation Amongst Settings And Game Scene From Menu

---
+++ Pawz n' Clawz Game Jam/Assets/Scripts/SceneNavigation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+    public void MenuButton()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+    }
+
+    public void SettingsButton()
+    {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Additive);
+    }
+
+    public void ManagerScene()
+    {
+        SceneManager.LoadSceneAsync(3, LoadSceneMode.Additive);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
