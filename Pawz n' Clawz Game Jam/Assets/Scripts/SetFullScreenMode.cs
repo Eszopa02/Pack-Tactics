a69b8001ea62260deb6e92a66cd22266cb60e862
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-07-13 23:39:07 -0500

    Player Can Now Select FullScreenMode

---
+++ Pawz n' Clawz Game Jam/Assets/Scripts/SetFullScreenMode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFullScreenMode : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        Screen.fullScreen = true;
+    }
+}
