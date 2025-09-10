cf4b9001fa0f8d0dc97f491fcb3260b928219a8d
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-09-10 18:24:42 -0500

    Revert "Moved everything into it's own scenes"
    This reverts commit d8dced5df356ad53029bc4e64d90abf93283658b.

--- Pack Tactics/Assets/Scripts/SceneNavigation.cs
+++ Pack Tactics/Assets/Scripts/SceneNavigation.cs
@@ -5,45 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class SceneNavigation : MonoBehaviour
 {
-    /* public void MenuButton()
-     {
-         SceneManager.LoadScene(1, LoadSceneMode.Additive);
-
-     }
-
-     public void PlayButton()
-     {
-         SceneManager.UnloadSceneAsync(1);
-         SceneManager.LoadSceneAsync(3, LoadSceneMode.Additive);
-     }
-
-     public void SettingsButton()
-     {
-         SceneManager.LoadSceneAsync(0, LoadSceneMode.Additive);
-     }
-
-     public void ManagerScene()
-     {
-         SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
-     }
-
-
-     public void QuitGame()
-     {
-         Application.Quit();
-     } */
-
-    public void GameScene()
+    public void MenuButton()
     {
-        SceneManager.LoadScene("Game Scene");
-        SceneManager.UnloadSceneAsync("Main Menu");
-        SceneManager.LoadScene("Manager Scene");
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+
     }
 
+<<<<<<< HEAD
     public void PlayerCharacterSelection()
     {
         SceneManager.LoadScene("Player Character Selection Scene");
         SceneManager.UnloadSceneAsync("Menu Scene");
     }
 
+=======
+    public void PlayButton()
+    {
+        SceneManager.UnloadSceneAsync(1);
+        SceneManager.LoadSceneAsync(3, LoadSceneMode.Additive);
+    }
+
+    public void SettingsButton()
+    {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Additive);
+    }
+
+    public void ManagerScene()
+    {
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+    }
+    
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+>>>>>>> parent of 04fe1976 (Scene Transition from Main Menu to Game)
 }
